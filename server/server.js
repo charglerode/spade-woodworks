b5893de9681588507ad2d9b6de915ec8249c85fe
charglerode <charglerode@gmail.com>
2025-02-15 10:48:10 -0800

    Additional gloabl error handling

--- server/server.js
+++ server/server.js
@@ -5,10 +5,25 @@ const app = require('./app');
 env.config({ path: './.env' });
 
 const db = process.env.DB.replace('<PASSWORD>', process.env.DB_PASS);
-mongoose.connect(db)
-.then(() => console.log('Connected to database!'));
+mongoose.connect(db).then(() => console.log('Connected to database!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('Unhandled Rejection! Shutting Down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('uncaughtException', (err) => {
+  console.log('Uncaught Exception! Shutting Down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
