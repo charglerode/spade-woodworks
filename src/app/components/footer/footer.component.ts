80c3cc7fac5c915efbf025c37da849d45453af0f
charglerode <charglerode@gmail.com>
2025-02-21 14:51:30 -0800

    Added randomization of quotes every 5 minutes

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { QuoteService } from '../../services/quote.service';
 
@@ -8,14 +8,25 @@ import { QuoteService } from '../../services/quote.service';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {
+export class FooterComponent implements OnDestroy {
   quote: { line: String; attribution: String } = {
     line: 'Give me six hours to chop down a tree and I will spend the first four sharpening the axe.',
     attribution: 'Abraham Lincoln',
   };
+  interval: any;
 
   constructor(private service: QuoteService) {
     this.getQuote();
+    this.interval = setInterval(
+      () => {
+        this.getQuote();
+      },
+      1000 * 60 * 5,
+    );
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
   }
 
   getQuote(): void {
