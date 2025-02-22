ae52582b95f9efa9f54a5ba9af9ec6aafb33d21b
charglerode <charglerode@gmail.com>
2025-02-21 21:37:53 -0800

    Minor copy and layout changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product.model';
 import { ProductService } from '../../services/product.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
