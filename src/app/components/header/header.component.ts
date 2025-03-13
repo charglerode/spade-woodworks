d1fe2079325ab66368105e10c92a42aae1bbff7d
charglerode <charglerode@gmail.com>
2025-03-13 00:34:36 -0700

    Added responsiveness to public facing pages

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { CartService } from '../../services/cart.service';
 
 @Component({
@@ -10,11 +10,32 @@ import { CartService } from '../../services/cart.service';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  @ViewChild('menu') menu: ElementRef | undefined;
+  @ViewChild('icon') icon: ElementRef | undefined;
+
   itemsInCart: number = 0;
 
-  constructor(private cartService: CartService) {
+  constructor(
+    private cartService: CartService,
+    private router: Router,
+  ) {
     this.cartService.cart$.subscribe((items) => {
       this.itemsInCart = items.length;
     });
+  }
+
+  toggleMenu(): void {
+    if (this.menu?.nativeElement.classList.contains('active')) {
+      this.menu?.nativeElement.classList.remove('active');
+      this.icon!.nativeElement.innerHTML = '<i class="fa-solid fa-bars"></i>';
+    } else {
+      this.menu?.nativeElement.classList.add('active');
+      this.icon!.nativeElement.innerHTML = '<i class="fa-solid fa-times"></i>';
+    }
+  }
+
+  menuSelected(page: string): void {
+    this.toggleMenu();
+    this.router.navigate([page]);
   }
 }
